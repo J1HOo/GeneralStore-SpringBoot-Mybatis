a38698884ce2be2e2a5a99bd1c9a2545b108a5c9
J1HOo <businessj1ho@gmail.com>
2025-02-27 12:51:32 +0900

    +

---
+++ UserRole/src/main/java/kh/edu/react/provider/dto/Clothes.java
@@ -0,0 +1,19 @@
+package kh.edu.react.provider.dto;
+
+import lombok.Data;
+
+@Data
+public class Clothes {
+    private int cId;
+    private String cName;
+    private String cCategory;
+    private String cBrand;
+    private String cColor;
+    private String cSize;
+    private String cMaterial;
+    private int cPrice;
+    private int cStock;
+    private String cGender;
+    private String cSeason;
+    private String cRegisterDate;
+}
